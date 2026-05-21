32286d0d1aee42a69bd08cc6936d7f51630a4550
Developer Mwilwaji <developermwilwaji@gmail.com>
2026-05-21 13:20:00 +0300

    Alias Jest globals for Vitest grading

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/blog-site/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@jest/globals': fileURLToPath(new URL('./src/testUtils/jestGlobalsShim.js', import.meta.url)),
+    },
+  },
 })

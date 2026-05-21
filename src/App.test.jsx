aa1a7ae005abdcc3efb9a5a62bba37c1a05126e5
Developer Mwilwaji <developermwilwaji@gmail.com>
2026-05-21 13:01:58 +0300

    Remove Jest globals import from tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,3 @@
-import { describe, expect, test } from '@jest/globals'
 import { render, screen, within } from '@testing-library/react'
 import App from './App'
 import About from './components/About'
